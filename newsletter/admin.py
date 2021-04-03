b9182ca8dfaa24ced14676c50b7e8b2d6d87952f
Nancy Chauhan <nancychn1@gmail.com>
2021-04-03 19:36:48 +0530

    Set up and activate models

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Newsletter, Subscriber, Issue, Subscription
+
+admin.site.register(Newsletter)
+admin.site.register(Subscriber)
+admin.site.register(Issue)
+admin.site.register(Subscription)
+
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Newsletter(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Subscriber(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=1024)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    newsletters = models.ManyToManyField(Newsletter, through='Subscription')
+
+    def __str__(self):
+        return self.email
+
+
+class Issue(models.Model):
+    content = models.TextField()
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Subscription(models.Model):
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subscriber.name + ' Subscribed to ' + self.newsletter.name
