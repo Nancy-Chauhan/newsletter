8b4e0b78f3cee5e4709d975251384915f0dd02b1
Nancy Chauhan <nancychn1@gmail.com>
2021-04-03 20:00:00 +0530

    Setup Django admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -2,8 +2,25 @@ from django.contrib import admin
 
 from .models import Newsletter, Subscriber, Issue, Subscription
 
-admin.site.register(Newsletter)
-admin.site.register(Subscriber)
 admin.site.register(Issue)
 admin.site.register(Subscription)
 
+
+class IssueAdmin(admin.StackedInline):
+    model = Issue
+    pass
+
+
+class SubscriptionAdmin(admin.StackedInline):
+    model = Subscription
+    pass
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    inlines = (IssueAdmin, SubscriptionAdmin)
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    inlines = (SubscriptionAdmin,)
