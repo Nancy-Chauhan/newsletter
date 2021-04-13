2d4a63fa8e1a212a27d894033f3ea6f5ed1bbaef
Nancy Chauhan <nancychn1@gmail.com>
2021-04-13 16:02:19 +0530

    Configure static files

--- newsletter_site/settings.py
+++ newsletter_site/settings.py
@@ -30,6 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('APP_DOMAIN')]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'newsletter_site.urls'
