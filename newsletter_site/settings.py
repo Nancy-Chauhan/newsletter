01318a9fff959d43f1f8b9c0a64d53aa8c3b9227
Nancy Chauhan <nancychn1@gmail.com>
2021-04-13 15:51:34 +0530

    Set ALLOWED_HOSTS

--- newsletter_site/settings.py
+++ newsletter_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('APP_DOMAIN')]
 
 # Application definition
 
