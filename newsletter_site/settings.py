17dff8efb80bc2594ca2acb39682b3f08ad53b90
Nancy Chauhan <nancychn1@gmail.com>
2021-04-13 16:32:04 +0530

    Set debug from env

--- newsletter_site/settings.py
+++ newsletter_site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = [os.getenv('APP_DOMAIN')]
 
